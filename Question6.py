ac09095f0fb556d0660597604273106448253fc8
Shmilowitz <sandwichmanden@hotmail.com>
2017-03-08 04:34:46 +0100

    Add files via upload
    Made changes to Question6

--- Question6.py
+++ Question6.py
@@ -1,42 +1,50 @@
-import numpy as np, matplotlib.pyplot as plt, webget, pandas as pd,collections
+import numpy as np, matplotlib.pyplot as plt, webget, pandas as pd,collections, csv
 
 vgsales = pd.read_csv('vgsales.csv',quotechar='"',skipinitialspace=True, delimiter=',')
 data = vgsales.as_matrix()#Which region has the most sales per person?
-
-
-# NA
-platform_roll_upNA = vgsales.groupby('Platform')['NA_Sales'].sum()
-idx_maxNA = platform_roll_upNA.idxmax()
-NAmsg = "North America's best selling platform is {} with {} Million sales"
-# print("North America\n", NAmsg.format(idx_maxNA, "%.2f" % platform_roll_upNA[idx_maxNA]), "\n")
-# EU
-platform_roll_upEU = vgsales.groupby('Platform')['EU_Sales'].sum()
-idx_maxEU = platform_roll_upEU.idxmax()
-EUmsg = "European's best selling platform is {} with {} Million sales"
-# print("Europe\n",EUmsg.format(idx_maxEU, "%.2f" % platform_roll_upEU[idx_maxEU]), "\n")
-# Japan
-platform_roll_upJP = vgsales.groupby('Platform')['JP_Sales'].sum()
-idx_maxJP = platform_roll_upJP.idxmax()
-JPmsg = "Japan's best selling platform is {} with {} Million sales"
-# print("Japan's\n",JPmsg.format(idx_maxJP, "%.2f" % platform_roll_upJP[idx_maxJP]), "\n")
 
 
 print("Which region has the most sales per person?")
 JPpop = 126_135_040
 NApop = 362_346_086
 EUpop = 506_279_458
-print('JP sales per person', platform_roll_upJP[idx_maxJP]*1_000_000 / JPpop)
-print('NA sales per person', platform_roll_upNA[idx_maxNA]*1_000_000 / NApop)
-print('Euro sales per person', platform_roll_upEU[idx_maxEU]*1_000_000 / EUpop)
 
+csv_file = csv.reader(open("vgsales.csv"))
+distEU = 0
+for row in csv_file:
+    _distEU = row[7]
+    try: 
+        _distEU = float(_distEU)
+    except ValueError:
+        _distEU = 0
+
+    distEU += _distEU
+csv_file = csv.reader(open("vgsales.csv"))
+distNA = 0
+for row in csv_file:
+    _distNA = row[6]
+    try: 
+        _distNA = float(_distNA)
+    except ValueError:
+        _distNA = 0
+
+    distNA += _distNA
+csv_file = csv.reader(open("vgsales.csv"))
+distJP = 0
+for row in csv_file:
+    _dist = row[8]
+    try: 
+        _dist = float(_dist)
+    except ValueError:
+        _dist = 0
+
+    distJP += _dist
 
 objects = [
 'Japan','North America','Europe'
 ]
-performance = [
-platform_roll_upJP[idx_maxJP]*1_000_000 / JPpop, platform_roll_upNA[idx_maxNA]*1_000_000 / NApop, platform_roll_upEU[idx_maxEU]*1_000_000 / EUpop
+performance = [distJP * 1_000_000 / JPpop, distNA * 1_000_000 / NApop, distEU * 1_000_000 / EUpop
 ]
-
 y_pos = np.arange(len(objects))
 plt.bar(y_pos, performance, align='center', alpha=0.5)
 plt.xticks(y_pos, objects, rotation=45)
